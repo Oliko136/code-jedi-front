08cb8c113273faa7c5a44857abf8e70d9abb780d
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-05-04 17:11:02 +0300

    welcome

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,21 +1,50 @@
 import { NavLink } from 'react-router-dom';
+import { WelcomeBg, WelcomeLogo } from './WelcomePage.styled';
+
+import WelcomeImgTel1x from './WelcomeImages/welcome-mob-1x.png';
+import WelcomeImgTel2x from './WelcomeImages/welcome-mob-2x.png';
+import WelcomeImgTab1x from './WelcomeImages/welcome-tab-1x.png';
+import WelcomeImgTab2x from './WelcomeImages/welcome-tab-2x.png';
+import WelcomeImgDesktop1x from './WelcomeImages/welcome-desk-1x.png';
+import WelcomeImgDesktop2x from './WelcomeImages/welcome-desk-2x.png';
 
 const WelcomePage = () => {
   return (
-    <>
+    <WelcomeBg>
       {/* Це лише заглушка. Можна буде прибрати/коригувати */}
+      <picture>
+        <source
+          media="(max-width: 375px)"
+          srcSet={`${WelcomeImgTel1x} 1x, ${WelcomeImgTel2x} 2x`}
+        />
 
-      <h1>WelcomePage</h1>
-      <button>
-        <NavLink to="/home">Home</NavLink>
-      </button>
+        <source
+          media="(max-width: 768px)"
+          srcSet={`${WelcomeImgTab1x} 1x, ${WelcomeImgTab2x} 2x`}
+        />
+
+        <source
+          media="(max-width: 1440px)"
+          srcSet={`${WelcomeImgDesktop1x} 1x, ${WelcomeImgDesktop2x} 2x`}
+        />
+
+        <img src={WelcomeImgTel1x} alt="User" />
+      </picture>
+
+      <WelcomeLogo>
+        <h1>Task Pro</h1>
+      </WelcomeLogo>
+
       <button>
         <NavLink to="/auth/register">Register</NavLink>
       </button>
       <button>
         <NavLink to="/auth/login">Login</NavLink>
       </button>
-    </>
+      <button>
+        <NavLink to="/home">Home</NavLink>
+      </button>
+    </WelcomeBg>
   );
 };
 
--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -1,5 +1,42 @@
 import styled from 'styled-components';
 
 export const WelcomeBg = styled.div`
-  background: --welcomeBgColor;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  max-width: 100%;
+  max-height: 100%;
+  background: var(--welcomeBgColor);
+
+  img {
+    margin-bottom: 14px;
+  }
+
+  @media screen (min-width: 768px) {
+    margin-bottom: 24px;
+  }
+`;
+
+export const WelcomeLogo = styled.div`
+  display: flex;
+  gap: 14px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 24px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 30px;
+  }
+
+  h1 {
+    font-size: 28px;
+    font-weight: 600;
+    letter-spacing: -0.04em;
+
+    @media screen and (min-width: 768px) {
+      font-size: 40px;
+    }
+  }
 `;
