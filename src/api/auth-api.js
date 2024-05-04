1ba724dc4bbe11d4d5e700859d5b63036ff1daa9
Oliko136 <olchick136@gmail.com>
2024-05-05 01:01:50 +0400

    Axios auth api services added

---
+++ src/api/auth-api.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { BASE_API_URL } from '../constants/base-url';
+
+axios.defaults.baseURL = BASE_API_URL;
+
+const setAuthHeader = token => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+}
+
+const clearAuthHeader = () => {
+    axios.defaults.headers.common.Authorization = '';
+}
+
+export async function register(credentials) {
+    const { data } = await axios.post('/auth/register', credentials);
+    setAuthHeader(data.token);
+    return data;
+}
+
+export async function logIn(credentials) {
+    const { data } = await axios.post('/auth/login', credentials);
+    setAuthHeader(data.token);
+    return data;
+}
+
+export async function logOut() {
+    await axios.post('/auth/logout');
+    clearAuthHeader();
+}
+
+export async function getCurrentUser(token) {
+    setAuthHeader(token);
+    try {
+        const { data } = await axios.get('/auth/current');
+        return data;
+    } catch (error) {
+        clearAuthHeader();
+        throw error;
+    }
+}
---
+++ src/api/user-api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { BASE_API_URL } from '../constants/base-url';
+
+axios.defaults.baseURL = BASE_API_URL;
+
+export async function updateUserInfo(body) {
+    const { data } = await axios.put('/users', body);
+    return data;
+}
+
+export async function updateUserTheme(theme) {
+    const { data } = await axios.patch('/users', theme);
+    return data;
+}
+
+export async function updateUserAvatar(file) {
+    const { data } = await axios.patch('/users/avatars', file, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+    });
+    return data;
+}
