b1a421f13f34c8088d3e4c5bf0e7b2b874da5d13
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-05-06 13:12:27 +0300

    commit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,9 @@
 //import { Suspense } from 'react';
+import Loader from 'components/Loader/Loader';
+import { MainContainer } from './HomePage.styled';
 
 const HomePage = () => {
-  return (
-    <>
-      {/* Це лише заглушка. Можна буде прибрати/коригувати */}
-
-      <h1>HomePage</h1>
-    </>
-  );
+  return <>{/* Це лише заглушка. Можна буде прибрати/коригувати */}</>;
 };
 
 export default HomePage;
