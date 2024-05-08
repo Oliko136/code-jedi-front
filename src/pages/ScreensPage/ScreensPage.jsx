713650b761906024e338edaabb99e174f7d98da0
Oliko136 <olchick136@gmail.com>
2024-05-08 17:33:37 +0400

    Update screens page

--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,6 +1,13 @@
 const ScreensPage = () => {
   return (
     <>
+      <p>
+        Before starting your project, it is essential{' '}
+        {/*<CreateBoard>*/}to create a board{/*</CreateBoard>*/} to visualize and track
+        all the necessary tasks and milestones. This board serves as a
+        powerful tool to organize the workflow and ensure effective
+        collaboration among team members.
+      </p>
     </>
   );
 };
