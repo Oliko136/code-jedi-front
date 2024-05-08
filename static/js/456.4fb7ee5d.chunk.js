63b60f2d6cd84992c98cfb25ffa09a36c013d3ce
Oliko136 <89538242+Oliko136@users.noreply.github.com>
2024-05-08 19:33:47 +0000

    Deploying to gh-pages from @ Oliko136/code-jedi-front@26580e5cc9cf473618f2a37215d52bc390ad9522 🚀

---
+++ static/js/456.4fb7ee5d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcode_jedi_front=self.webpackChunkcode_jedi_front||[]).push([[456],{4456:(e,a,s)=>{s.r(a),s.d(a,{default:()=>o});var t=s(579);const o=()=>(0,t.jsx)(t.Fragment,{children:(0,t.jsxs)("p",{children:["Before starting your project, it is essential"," ","to create a board"," to visualize and track all the necessary tasks and milestones. This board serves as a powerful tool to organize the workflow and ensure effective collaboration among team members."]})})}}]);
+//# sourceMappingURL=456.4fb7ee5d.chunk.js.map
