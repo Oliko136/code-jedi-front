776374459ddd197b59d8d661c05aad86b327138d
Oliko136 <olchick136@gmail.com>
2024-05-09 18:13:08 +0400

    Basic slice structure added

--- src/redux/boards/boards-slice.js
+++ src/redux/boards/boards-slice.js
@@ -1,9 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-//import { getAllBoards, getOneBoard, deleteBoard } from "./boards-operations.js";
-//import { pending, rejected } from "../../helpers/redux-functions.js";
+import { getAllBoards, createBoard, getOneBoard, updateBoard, deleteBoard } from "./boards-operations.js";
+import { pending, rejected } from "../../helpers/redux-functions.js";
+import { get } from "react-hook-form";
 
 const initialState = {
     boards: [],
+    totalBoards: 0,
     currentBoard: {},
     isLoading: false,
     error: null
@@ -12,7 +14,24 @@ const initialState = {
 const boardSlice = createSlice({
     name: "boards",
     initialState,
-    extraReducers: builder => {}
+    extraReducers: builder => {
+        builder
+            .addCase(getAllBoards.pending, pending)
+            .addCase(getAllBoards.fulfilled)
+            .addCase(getAllBoards.rejected, rejected)
+            .addCase(createBoard.pending, pending)
+            .addCase(createBoard.fulfilled)
+            .addCase(createBoard.rejected, rejected)
+            .addCase(getOneBoard.pending, pending)
+            .addCase(getOneBoard.fulfilled)
+            .addCase(getOneBoard.rejected, rejected)
+            .addCase(updateBoard.pending, pending)
+            .addCase(updateBoard.fulfilled)
+            .addCase(updateBoard.rejected, rejected)
+            .addCase(deleteBoard.pending, pending)
+            .addCase(deleteBoard.fulfilled)
+            .addCase(deleteBoard.rejected, rejected)
+    }
 });
 
 export const boardReducer = boardSlice.reducer;
