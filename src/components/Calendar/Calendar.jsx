599175604c10fdc6b66eda9a2687d8bb8d37247f
NetVis <mr.forzioffical@gmail.com>
2024-05-11 00:24:35 +0200

    Calendar implementation
    Developed a Calendar component, however, I cannot verify its work as it was written without having data and function for handling data change. Also, I don't know how to connect svg to this library.

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,46 @@
+import Icon from 'components/Icon/Icon';
+import DatePicker from 'react-datepicker';
+import * as Styled from './Calendar.styled';
+import { forwardRef } from 'react';
+
+const Calendar = ({ date, changeDate }) => {
+  const CustomInput = forwardRef(({ value, onClick }, ref) => (
+    <Styled.Button type="button" onClick={onClick} ref={ref}>
+      {value}
+      <Styled.ButtonSvg>
+        <Icon name="chevron-down" />
+      </Styled.ButtonSvg>
+    </Styled.Button>
+  ));
+
+  const dateFormatChange = date => {
+    if (!(date instanceof Date)) {
+      return 'MMMM dd, yyyy';
+    }
+    const today = new Date();
+    if (
+      date.getDate() === today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
+    ) {
+      return "'Today', MMMM dd";
+    } else {
+      return 'EEEE, MMMM dd';
+    }
+  };
+
+  return (
+    <Styled.StyledDatePickerContainer>
+      <DatePicker
+        className="datePicker"
+        selected={date}
+        onChange={changeDate}
+        minDate={new Date()}
+        dateFormat={dateFormatChange(date)}
+        customInput={<CustomInput />}
+      />
+    </Styled.StyledDatePickerContainer>
+  );
+};
+
+export default Calendar;
