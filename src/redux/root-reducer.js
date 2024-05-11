abab7d10ad93ca570a0b5f4b1dfd0711f2f49d85
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-05-11 17:09:27 +0300

    Init Filter
    Криво але наче працює.
    Потребуэ доопрацювання...

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import { authReducer } from './auth/auth-slice';
 import { themeReducer } from './theme/theme-slice';
 import { needhelpReducer } from './needhelp/needhelpSlice';
 import { boardReducer } from './boards/boards-slice';
+import {filterReducer} from './filter/filter-slice';
 
 const persistConfig = {
   key: 'root',
@@ -15,12 +16,14 @@ const persistConfig = {
 const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 const persistedThemeReducer = persistReducer(persistConfig, themeReducer);
 const persistedneedhelpReducer = persistReducer(persistConfig, needhelpReducer);
+const persistedFilterReducer = persistReducer(persistConfig, filterReducer);
 
 const rootReducer = combineReducers({
   auth: persistedAuthReducer,
   theme: persistedThemeReducer,
   boards: boardReducer,
   needhelp: persistedneedhelpReducer,
+  filter: persistedFilterReducer,
 });
 
 export default rootReducer;
