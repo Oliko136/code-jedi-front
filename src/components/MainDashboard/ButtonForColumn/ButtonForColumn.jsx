3ac1fa609c692e051f8269e38d57e0b55500032c
ira089 <gvir2015@gmail.com>
2024-05-12 16:34:39 +0300

    updBtnAddColumn

--- src/components/MainDashboard/ButtonForColumn/ButtonForColumn.jsx
+++ src/components/MainDashboard/ButtonForColumn/ButtonForColumn.jsx
@@ -1,14 +1,39 @@
-import { AddColumnButton, IconPlus } from './ButtonForColumn.styled';
-import sprite from '../../../assets/svg/sprite.svg';
+// import { AddColumnButton, IconPlus } from './ButtonForColumn.styled';
+// import sprite from '../../../assets/svg/sprite.svg';
+import ColumnModal from 'components/Modal/ColumnModal/ColumnModal';
+import { useState } from 'react';
+import Icon from '../../Modal/Modal/Modal';
+import  {IconWrap, PlusButton, ButtonSend} from './ButtonForColumn.styled'
 
 const ButtonForColumn = () => {
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => setShowModal(prevShowModal => !prevShowModal)
+
   return (
-    <AddColumnButton type="button">
+    <>
+    {/* <AddColumnButton type="button" onClick={toggleModal}>
       <IconPlus>
         <use href={`${sprite}#plus`}></use>
       </IconPlus>
       Add another column
-    </AddColumnButton>
+    </AddColumnButton> */}
+     <ButtonSend type="submit" onClick={toggleModal}>
+            <PlusButton>
+                <IconWrap>
+                <Icon width={14}
+                      height={14}
+                      fillColor={'none'}
+                      strokeColor={'var(--btn-add-column)'}
+                      name={"icon-plus"}
+                      />
+                </IconWrap>
+            
+                Add another column
+            </PlusButton>
+          </ButtonSend>
+    {showModal && (<ColumnModal showModal={setShowModal} />)}
+    </>
+    
   );
 };
 
