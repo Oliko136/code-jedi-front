9740f846eea9fd8053e56a92a8c1a1447e94993c
Oliko136 <olchick136@gmail.com>
2024-05-13 16:51:25 +0400

    Updated current board

--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,9 +1,30 @@
-
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import HeaderDashboard from 'components/HeaderDashboard/HeaderDashboard';
 import MainDashboard from 'components/MainDashboard/MainDashboard';
-import {ContainerScreensPage} from './ScreenPage.styled'
+import { getBoardByIdThunk } from '../../redux/boards/boards-operations.js';
+import { selectBoards } from '../../redux/boards/boards-selectors.js';
+import { ContainerScreensPage } from './ScreenPage.styled'
 
 const ScreensPage = () => {
+  const boards = useSelector(selectBoards);
+  const { boardName } = useParams();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    try {
+      if (boardName) {
+      const board = boards.find(board => board.title === boardName);
+      const boardId = board._id;
+        dispatch(getBoardByIdThunk(boardId));
+      }
+      dispatch(getBoardByIdThunk(boards[0]._id));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [dispatch, boardName, boards]);
 
   return(
     <ContainerScreensPage>
