6a1edc7f329541b69c846ebd119b64bf55bf8762
Oliko136 <olchick136@gmail.com>
2024-05-15 03:01:19 +0400

    card deleting upd

--- src/components/MainDashboard/TasksCard/TasksCardList.jsx
+++ src/components/MainDashboard/TasksCard/TasksCardList.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { selectCards } from "../../../redux/cards/cards-selectors.js";
 import { selectCurrentBoard } from "../../../redux/boards/boards-selectors.js";
-import { getAllCardsThunk } from "../../../redux/cards/cards-operations.js";
+import { getAllCardsThunk, deleteCardThunk } from "../../../redux/cards/cards-operations.js";
 import TasksCard from "./TasksCard";
 import { CardContainer } from "./TasksCardList.styled.jsx";
 import {getFilter} from '../../../redux/filter/filter-selectors.js'
@@ -14,26 +14,30 @@ const TasksCardList = ({columnId}) => {
     const filter = useSelector(getFilter);
     const filteredCards = columnCards.filter(({priority}) => priority.toLowerCase() === filter.toLowerCase() )
     const cards = filter === 'all' ? columnCards : filteredCards; 
-      
     
-
+  
     const board = useSelector(selectCurrentBoard);
     const { _id: boardId } = board;
 
-   
-
-    
     const dispatch = useDispatch();
     
     useEffect(() => {
-        dispatch(getAllCardsThunk({ boardId, columnId }));
+      dispatch(getAllCardsThunk({ boardId, columnId }));
     }, [dispatch, boardId, columnId]);
+  
+  const handleDeleteCard = (boardId, columnId, card) => {
+    try {
+      dispatch(deleteCardThunk({ boardId, columnId, cardId: card._id }));
+      } catch (error) {
+      return error.message;
+    }
+  }
 
   return (
     <CardContainer>
       {cards.length > 0 &&
         cards.map(card => (
-          <TasksCard key={card._id} card={card} columnId={columnId} />
+          <TasksCard key={card._id} card={card} columnId={columnId} onDelete={()=> handleDeleteCard(boardId, columnId, card )}/>
         ))}
     </CardContainer>
   );
