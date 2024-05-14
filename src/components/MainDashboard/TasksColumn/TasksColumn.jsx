01bc906591704bb0d9eb8ced37d48c47d407964b
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2024-05-14 03:20:52 +0300

    update column deletion
    update column deletion

--- src/components/MainDashboard/TasksColumn/TasksColumn.jsx
+++ src/components/MainDashboard/TasksColumn/TasksColumn.jsx
@@ -1,15 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { selectСolumns } from '../../../redux/column/column-selectors.js';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 import { DivForColumns } from './TasksColumn.styled.jsx';
 import TasksColumnItem from './TasksColumnItem.jsx';
 import { selectCurrentBoard } from '../../../redux/boards/boards-selectors.js';
-import { getAllColumnsThunk } from '../../../redux/column/column-operations.js';
-import { useEffect } from 'react';
+import { selectСolumns } from '../../../redux/column/column-selectors.js';
+import {
+  deleteColumnThunk,
+  getAllColumnsThunk,
+} from '../../../redux/column/column-operations.js';
 
 const TasksColumn = () => {
   const columns = useSelector(selectСolumns);
   const board = useSelector(selectCurrentBoard);
-
   const { _id } = board;
 
   const dispatch = useDispatch();
@@ -18,11 +21,36 @@ const TasksColumn = () => {
     dispatch(getAllColumnsThunk(_id));
   }, [_id, dispatch]);
 
+  const TOASTER = {
+    style: {
+      border: '2px solid #bedbb0',
+      backgroundColor: '#1f1f1f',
+      color: '#fff',
+      textAlign: 'center',
+    },
+    position: 'top-center',
+    duration: 2000,
+  };
+
+  const handleDeleteColumn = async colunmId => {
+    try {
+      dispatch(deleteColumnThunk({ boardId: _id, id: colunmId }));
+      toast('You have successfully deleted the column ✅', TOASTER);
+      dispatch(getAllColumnsThunk(_id));
+    } catch (error) {
+      return error.message;
+    }
+  };
+
   return (
     <DivForColumns>
       {columns.length > 0 &&
         columns.map(column => (
-          <TasksColumnItem key={column._id} column={column} />
+          <TasksColumnItem
+            key={column._id}
+            column={column}
+            onDeleteColumn={() => handleDeleteColumn(column._id)}
+          />
         ))}
     </DivForColumns>
   );
