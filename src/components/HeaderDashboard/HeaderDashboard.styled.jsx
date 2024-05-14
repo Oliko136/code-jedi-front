0b4f117075d1733ed3922554a73f2286289ed79a
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-05-14 19:35:49 +0300

    commit

--- src/components/HeaderDashboard/HeaderDashboard.styled.jsx
+++ src/components/HeaderDashboard/HeaderDashboard.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const ContainerHeaderBoard = styled.div`
-display: flex;
+  display: flex;
   align-items: center;
   justify-content: space-between;
   padding-bottom: 10px;
