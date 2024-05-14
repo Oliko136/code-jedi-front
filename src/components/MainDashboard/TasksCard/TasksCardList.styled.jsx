57f933bbd73c3b4948025570c86af6c672e8f944
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-05-14 22:44:35 +0300

    commit

--- src/components/MainDashboard/TasksCard/TasksCardList.styled.jsx
+++ src/components/MainDashboard/TasksCard/TasksCardList.styled.jsx
@@ -1,7 +1,23 @@
 import styled from 'styled-components';
 
 export const CardContainer = styled.div`
-  max-height: 65vh;
-  overflow-x: visible;
+  max-height: 63vh;
+  height: 100%;
   overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    position: absolute;
+    left: 5px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: rgba(255, 255, 255, 0.5);
+    border-radius: 12px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(12, 12, 12, 0.5);
+    border-radius: 12px;
+  }
 `;
